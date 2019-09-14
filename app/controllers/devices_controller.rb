a11641cabf6a31e897f9eedc870b34cfc51a2378
Jason Dixon <jasondixonmail@gmail.com>
2019-09-14 13:02:32 -0700

    created forms and handling to add new locations and devices. started but still wip for form and handling of new reading.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,10 +1,40 @@
 class DevicesController < ApplicationController
-
-    def new
-    end
+    before_action :set_device, only: [:show, :edit, :update, :destroy]
 
     def index
         @devices = Device.all
     end
 
+    def show
+    end
+
+    def new
+        @device = Device.new
+    end
+
+    def create
+        @device = Device.new(device_params)
+    
+        respond_to do |format|
+          if @device.save
+            format.html { redirect_to @device, notice: 'Device was successfully created.' }
+            format.json { render :show, status: :created, device: @device }
+          else
+            format.html { render :new }
+            format.json { render json: @device.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+    
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_device
+      @device = Device.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def device_params
+      params.require(:device).permit(:id, :location_id)
+    end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,40 @@
 class LocationsController < ApplicationController
-    
-    def new
-    end
+    before_action :set_location, only: [:show, :edit, :update, :destroy]
 
     def index
         @locations = Location.all
     end
 
+    def show
+    end
     
+    def new
+        @location = Location.new
+    end
 
+    
+    def create
+        @location = Location.new(location_params)
+    
+        respond_to do |format|
+          if @location.save
+            format.html { redirect_to @location, notice: 'Location was successfully created.' }
+            format.json { render :show, status: :created, location: @location }
+          else
+            format.html { render :new }
+            format.json { render json: @location.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+    
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_location
+        @location = Location.find(params[:id])
+      end
+  
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def location_params
+        params.require(:location).permit(:name)
+      end
 end
